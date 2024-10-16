40a298ed7bd14a6d9f433fe007dd874e8ec477e8
dayounglee <verdantjuly@kakao.com>
2024-10-16 14:54:53 +0900

    [Feat] useCallback

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { MetaContext, PageContext, PostListContext } from "./CafeSearcher";
 import "./SearchBar.css";
@@ -8,23 +8,29 @@ export default function SearchBar() {
   const { page } = useContext(PageContext);
   const { setPostList } = useContext(PostListContext);
   const { setMeta } = useContext(MetaContext);
-  const [isClicked, setIsClicked] = useState(false);
-  const fetchData = async () => {
-    const response = await axios.get("https://dapi.kakao.com/v2/search/cafe", {
-      headers: {
-        Authorization: `KakaoAK ${process.env.REACT_APP_REST_API}`,
-      },
-      params: { query, page, size: 5 },
-    });
-    setPostList(response.data.documents);
-    setMeta(response.data.meta);
-  };
-  useEffect(() => {
+
+  const stableFetchData = useCallback(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://dapi.kakao.com/v2/search/cafe",
+        {
+          headers: {
+            Authorization: `KakaoAK ${process.env.REACT_APP_REST_API}`,
+          },
+          params: { query, page, size: 5 },
+        }
+      );
+      setPostList(response.data.documents);
+      setMeta(response.data.meta);
+    };
     if (query) {
       fetchData();
-      setIsClicked(false);
     }
-  }, [isClicked, page]);
+  }, [query, page, setMeta, setPostList]);
+
+  useEffect(() => {
+    stableFetchData();
+  }, [page, query, stableFetchData]);
 
   return (
     <nav>
@@ -39,7 +45,7 @@ export default function SearchBar() {
           type="submit"
           onClick={(event) => {
             event.preventDefault();
-            setIsClicked(true);
+            stableFetchData();
           }}
         >
           검색
